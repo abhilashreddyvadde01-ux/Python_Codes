91c41ac140754f063528e52ffeea1a36cf77c4f8
Abhilash Reddy Vadde <abhilash.vadde@acuvate.com>
2026-08-21 17:23:52 +0530

    Set questions

---
+++ sets/five.py
@@ -0,0 +1,4 @@
+# Given {1, 2, 3, 4} and {3, 4, 5}, find the elements present only in the first set.
+set1 = {1,2,3,4}
+set2 = {3,4,5}
+print(set1.difference(set2))
---
+++ sets/nine.py
@@ -0,0 +1,4 @@
+# Create a set and remove all its elements using clear().
+set1 = {1,2,3,4}
+set1.clear()
+print(set1)
